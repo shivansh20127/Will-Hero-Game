d00d360694ea11427ffc380be31fa2f145cf96a9
Ojus Singhal <ojus.singhal17@gmail.com>
2021-12-08 20:19:53 +0530

    added login page

--- src/main/java/com/example/willherogame/Application.java
+++ src/main/java/com/example/willherogame/Application.java
@@ -1,19 +1,20 @@
 package com.example.willherogame;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application
+{
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 

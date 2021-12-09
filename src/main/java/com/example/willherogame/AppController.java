0a457b6fe001d54d91ec0a3c991bb10cc3e5735c
Shivansh Choudhary <shivansh20127@iiitd.ac.in>
2021-12-09 18:35:35 +0530

    Added Settings Page.

--- src/main/java/com/example/willherogame/AppController.java
+++ src/main/java/com/example/willherogame/AppController.java
@@ -2,17 +2,25 @@ package com.example.willherogame;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class AppController
 {
+    @FXML
     public void showCreateAccountPage(ActionEvent event) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("create-account.fxml"));
@@ -21,7 +29,8 @@ public class AppController
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
     }
-    
+
+    @FXML
     public void showLoginPage(Event event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
         AnchorPane anchorPane = fxmlLoader.load();
@@ -29,7 +38,7 @@ public class AppController
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
     }
-    
+    @FXML
     public void showMainPage(Event event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-page.fxml"));
         AnchorPane anchorPane = fxmlLoader.load();
@@ -37,7 +46,7 @@ public class AppController
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
     }
-    
+    @FXML
     public void showSavedGames(Event event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("show-saved-games.fxml"));
         ScrollPane scrollPane = fxmlLoader.load();
@@ -45,5 +54,75 @@ public class AppController
         Scene scene = new Scene(scrollPane);
         stage.setScene(scene);
     }
-    
+
+    @FXML
+    public void showsettings(MouseEvent event) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("temp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage1 = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage1.setTitle("Settings screen");
+        stage1.setScene(scene);
+        stage1.show();
+    }
+
+    @FXML
+    private ImageView musicicon;
+    private boolean music=true;
+    @FXML
+    public void no_music(MouseEvent event) throws IOException
+    {
+        Image newimage;
+        if (music)
+        {
+            newimage= new Image(getClass().getResourceAsStream("Images/no_music_icon.png"));
+            music = false;
+        }
+        else
+        {
+            newimage= new Image(getClass().getResourceAsStream("Images/music_icon.png"));
+            music = true;
+        }
+        musicicon.setImage(newimage);
+    }
+
+    @FXML
+    private ImageView soundicon;
+    private boolean sound=true;
+    @FXML
+    public void no_sound(MouseEvent event) throws IOException
+    {
+        Image newimage;
+        if(sound)
+        {
+            newimage= new Image(getClass().getResourceAsStream("Images/no_sound_icon.png"));
+            sound = false;
+        }
+        else
+        {
+            newimage= new Image(getClass().getResourceAsStream("Images/sound_icon.png"));
+            sound= true;
+        }
+        soundicon.setImage(newimage);
+    }
+
+    @FXML
+    private Button logoutbutton;
+    @FXML
+    private AnchorPane scenePane;
+    Stage stage;
+    public void logout(ActionEvent event)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?:");
+
+        if(alert.showAndWait().get() == ButtonType.OK)
+        {
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("You successfully logged out!");
+            stage.close();
+        }
+    }
 }
--- src/main/java/com/example/willherogame/Application.java
+++ src/main/java/com/example/willherogame/Application.java
@@ -1,7 +1,10 @@
 package com.example.willherogame;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,15 +12,33 @@ import java.io.IOException;
 public class Application extends javafx.application.Application
 {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-    }
 
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
+    }
+    public void logout(Stage stage)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?:");
+
+        if(alert.showAndWait().get() == ButtonType.OK)
+        {
+            System.out.println("You successfully logged out!");
+            stage.close();
+        }
+    }
     public static void main(String[] args) {
         launch();
     }
